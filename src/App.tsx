e8925c8287b2cbb8d9cb8a40c0b98ddcdcadbf0d
Meenashi <jeyanthi_sm@yahoo.com>
2023-04-18 14:23:40 +0100

    Implemented all tasks

--- src/App.tsx
+++ src/App.tsx
@@ -1,33 +1,95 @@
-import { useEffect, useState } from 'react';
-import { getAreaData } from './api';
-import { Place } from './types/Area';
-import './App.css';
+import { SetStateAction, useEffect, useState } from "react";
+import { getAreaData } from "./api";
+import { Place } from "./types/Area";
+import "./App.css";
+import Box from "@mui/material/Box";
+import Card from "@mui/material/Card";
+import CardContent from "@mui/material/CardContent";
+import Typography from "@mui/material/Typography";
 
 function App() {
-	const [areas, setAreas] = useState<Place[]>([]);
+  const [areas, setAreas] = useState<Place[]>([]);
+  const [inputOuterPostCode, setInputOuterPostCode] = useState<string>();
 
-	const load = async () => {
-		try {
-			const areaData = await getAreaData();
+  const load = async (inputPostCode: string) => {
+    try {
+      console.log(inputPostCode);
+      const areaData = await getAreaData(inputPostCode);
+      areas.concat(areaData);
+      console.log(areaData);
+      setAreas(areaData);
+    } catch (error) {
+      window.alert("todo: fix app");
+      console.log(`error ${error}`);
+    }
+  };
 
-			areas.concat(areaData);
+  const myDebounceFunction = (callBackFunction: Function, delay: number) => {
+    let timeOutId: number | undefined;
+    return function (...args: any) {
+      if (timeOutId) {
+        clearTimeout(timeOutId);
+      }
+      setTimeout(() => {
+        callBackFunction(...args);
+      }, delay);
+    };
+  };
 
-			setAreas(areas);
-		} catch (error) {
-			window.alert('todo: fix app');
-		}
-	};
+  useEffect(() => {
+    load(inputOuterPostCode as string);
+  }, [inputOuterPostCode]);
 
-	useEffect(() => {
-		load();
-	}, []);
+  const inputMyHandler = myDebounceFunction((e: any) => {
+    setInputOuterPostCode(e.target.value);
+  }, 1000);
 
-	return (
-		<div className='App'>
-			<h1>Postcoders</h1>
-			<h2>{`Areas for BB10: ${areas.length}`}</h2>
-		</div>
-	);
+  return (
+    <div className="App">
+      <h1>Postcoders</h1>
+
+      <input
+        type="text"
+        onChange={inputMyHandler}
+        // onChange={(e) => setInputOuterPostCode(e.target.value)}
+      />
+      <>
+        {areas &&
+          areas.length &&
+          areas.map((elem) => {
+            return (
+              <>
+                <Card sx={{ minWidth: 275 }}>
+                  <CardContent>
+                    <Typography variant="h5" component="div">
+                      Place Name :{elem["place name"]}
+                    </Typography>
+                    <Typography
+                      sx={{ fontSize: 14 }}
+                      color="text.secondary"
+                      gutterBottom
+                    >
+                      Latitude : {elem.latitude}
+                    </Typography>
+                    <Typography
+                      sx={{ fontSize: 14 }}
+                      color="text.secondary"
+                      gutterBottom
+                    >
+                      Longitude : {elem.longitude}
+                    </Typography>
+                    <Typography variant="h5">State : {elem.state}</Typography>
+                    <Typography variant="body2">
+                      State Abbreviation :{elem["state abbreviation"]}
+                    </Typography>
+                  </CardContent>
+                </Card>
+              </>
+            );
+          })}
+      </>
+    </div>
+  );
 }
 
 export default App;
--- src/api/index.ts
+++ src/api/index.ts
@@ -1,8 +1,13 @@
-import axios from 'axios';
-import { Place } from '../types/Area';
+import axios from "axios";
+import { Place } from "../types/Area";
 
-export const getAreaData = async () => {
-	const { data } = await axios.get('https://api.zipp🐘opotam.us/GB/bb8');
-
-	return data.places as Array<Place>;
+export const getAreaData = async (inputOuterPostCode: string) => {
+  console.log(inputOuterPostCode);
+  const { data } = await axios.get(
+    `https://api.zippopotam.us/GB/${inputOuterPostCode}`
+  );
+  //   const { data } = await axios.get(
+  // "https://api.xn--zippopotam-9p26i.us/GB/bb8"
+  //   );
+  return data.places as Array<Place>;
 };
